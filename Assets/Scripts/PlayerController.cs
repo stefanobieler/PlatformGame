667d950dff1a635921d7b4b542f6318ed19ac7bf
Stefano Bieler <stefanobieler@gmail.com>
2022-01-09 22:51:58 -0800

    Finished movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
-    PlayerInputAction playerInput;
-    InputAction move;
-    InputAction climb;
+    [SerializeField] private float movementSpeed = 5.0f;
+    [SerializeField] private float jumpForce = 5.0f;
+
+    private PlayerInputAction playerInput;
+    private InputAction move;
+    private InputAction climb;
+
+    private Rigidbody2D playerRigidBody;
+    private CapsuleCollider2D playerCollider;
+    private Animator anim;
+
 
     private void Awake(){
         playerInput = new PlayerInputAction();
@@ -17,6 +27,7 @@ public class PlayerController : MonoBehaviour
     private void SetupInput(){
         //get the jump input
         playerInput.Player.Jump.performed += Jump;
+        playerInput.Player.Jump.Enable();
         //Setup movement input
         move = playerInput.Player.Move;
         move.Enable();
@@ -24,12 +35,32 @@ public class PlayerController : MonoBehaviour
         climb = playerInput.Player.Climb;
     }
 
+    private void FixedUpdate(){
+        MovePlayer();
+    }
+
+    private void MovePlayer(){
+        float dir = move.ReadValue<float>();
+        Vector2 newVelocity = new Vector2(dir * movementSpeed, playerRigidBody.velocity.y);
+        playerRigidBody.velocity = newVelocity;
+        bool isRunning = Mathf.Abs(dir) >= Mathf.Epsilon ? true : false;
+        anim.SetBool("isRunning", isRunning);
+        if(isRunning) transform.localScale = new Vector2(Mathf.Sign(playerRigidBody.velocity.x), 1.0f);
+    }
+
     private void Jump(InputAction.CallbackContext obj)
     {
+        if(!playerCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) return;
+
+        Vector2 currentVelocity = playerRigidBody.velocity;
+        playerRigidBody.velocity += new Vector2(0.0f, jumpForce);
         
     }
 
     private void OnEnable(){
         SetupInput();
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        playerCollider = GetComponent<CapsuleCollider2D>();
     }
 }
