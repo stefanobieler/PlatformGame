a37220dbde9b89b2ddd76005d5ad8efcf2ea2494
Stefano Bieler <stefanobieler@gmail.com>
2022-01-09 20:46:57 -0800

    added controls, collider, and rigidbody to the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    PlayerInputAction playerInput;
+    InputAction move;
+    InputAction climb;
+
+    private void Awake(){
+        playerInput = new PlayerInputAction();
+    }
+
+    private void SetupInput(){
+        //get the jump input
+        playerInput.Player.Jump.performed += Jump;
+        //Setup movement input
+        move = playerInput.Player.Move;
+        move.Enable();
+        //set up climbing input
+        climb = playerInput.Player.Climb;
+    }
+
+    private void Jump(InputAction.CallbackContext obj)
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnEnable(){
+        SetupInput();
     }
 }
